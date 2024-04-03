505b692bdbf10b417ecaa5f1b6b54a69a1ffa35e
Hrushikesh-2004 <gujjetihrushikesh123@gmail.com>
2024-04-03 11:58:34 +0530

    Added Routes for workouts

--- Backend/sever.js
+++ Backend/sever.js
@@ -1,7 +1,23 @@
+// require('dotenv').config()
+
 const express = require('express')
+const workoutRoutes = require('./Routes/workouts')
 
-const app = express();
+// express app
+const app = express()
 
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+// listen for requests
 app.listen(5000, () => {
-    console.log('Sever is listening at port 5000');
-} )
+  console.log('listening on port')
+})
